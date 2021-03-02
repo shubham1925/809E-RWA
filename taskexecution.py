03032ef6ea617650eb93b1a383bc5370a2434453
Shubham Sonawane <shubham.sonawane@gmail.com>
2021-03-01 22:21:31 -0500

    Update taskexecution.py

--- taskexecution.py
+++ taskexecution.py
@@ -16,16 +16,41 @@ import taskplanning
 # taskplanning.red_needed, taskplanning.green_needed, taskplanning.blue_needed = red_needed, green_needed, blue_needed
 # taskplanning.red_remaining, taskplanning.green_remaining, taskplanning.blue_remaining = red_remaining, green_remaining, \
 #                                                                                         blue_remaining
-
+# Initialize robot at the home position
 taskplanning.go_home()
-taskplanning.move_to_tray()
-taskplanning.pick("red")
-taskplanning.move_to_bin()
-taskplanning.place("red")
+
+flag_red = False
+flag_green = False
+flag_blue = False
+if taskplanning.red_remaining <= 0:
+    print("Red parts already available in the tray, no need to pick up from bins")
+    flag_red = True
+
+if taskplanning.green_remaining <= 0:
+    print("Green parts already available in the tray, no need to pick up from bins")
+    flag_green = True
+
+if taskplanning.blue_remaining <= 0:
+    print("Blue parts already available in the tray, no need to pick up from bins")
+    flag_blue = True
+
+while not(flag_red):
+    taskplanning.move_to_bin()
+    taskplanning.pick("red")
+    taskplanning.move_to_tray()
+    rem_red = taskplanning.place("red")
+    # print("Baburao")
+    print("rem_red: " + str(rem_red))
+# taskplanning.move_to_bin()
+# taskplanning.pick("red")
+# taskplanning.move_to_tray()
+# rem_red = taskplanning.place("red")
+# print("rem_red: " + str(rem_red))
+    if rem_red == 0:
+        print("Exit")
+        flag_red = True
+    taskplanning.go_home()
+
+
 print(taskplanning.environment.items())
 # We will complete the kit in red, green blue order always
-
-
-
-
-
