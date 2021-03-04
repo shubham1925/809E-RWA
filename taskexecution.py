757c75ee03e862fd29c3de1f3bbffe9d2994dd92
Shubham Sonawane <shubham.sonawane@gmail.com>
2021-03-03 22:24:26 -0500

    Update taskexecution.py

--- taskexecution.py
+++ taskexecution.py
@@ -4,6 +4,7 @@ from termcolor import colored
 # Initialize robot at the home position
 taskplanning.go_home()
 
+# Set the color completion flags to False
 flag_red = False
 flag_green = False
 flag_blue = False
@@ -20,7 +21,6 @@ if taskplanning.blue_remaining <= 0:
     print("Blue parts already available in the tray, no need to pick up from bins")
     flag_blue = True
 
-# We will complete the kit in red, green blue order always
 while not flag_red:
     print(colored('Starting with red parts..', 'red'))
     taskplanning.move_to_bin()
@@ -29,7 +29,6 @@ while not flag_red:
     rem_red = taskplanning.place("red")
     print("\n")
     if rem_red == 0:
-        print("Exit")
         flag_red = True
     taskplanning.go_home()
 
@@ -40,7 +39,6 @@ while not flag_green:
     taskplanning.move_to_tray()
     rem_green = taskplanning.place("green")
     if rem_green == 0:
-        print("Exit")
         flag_green = True
     taskplanning.go_home()
 
@@ -51,7 +49,6 @@ while not flag_blue:
     taskplanning.move_to_tray()
     rem_green = taskplanning.place("blue")
     if rem_green == 0:
-        print("Exit")
         flag_blue = True
     taskplanning.go_home()
 
@@ -67,5 +64,3 @@ print(f'Parts remaining in blue bin: {taskplanning.environment["PartsInBins"]["b
 print(f'Parts in red kit: {taskplanning.environment["PartsInKit"]["red_in_kit"]}')
 print(f'Parts in green kit: {taskplanning.environment["PartsInKit"]["green_in_kit"]}')
 print(f'Parts in blue kit: {taskplanning.environment["PartsInKit"]["blue_in_kit"]}\n')
-
-print(taskplanning.environment.items())
